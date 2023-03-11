05f6085c529453a8fc770541a250a0d0adeeb215
swifteralex <swifteralexc@gmail.com>
2023-03-10 18:44:16 -0800

    Created batch script for running application on Windows

---
+++ RunApplication.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+ECHO Starting virtual environment...
+cd django-env-win/Scripts
+call activate.bat
+
+ECHO Installing Django...
+py -m pip install Django
+
+ECHO Running server...
+cd ../../titanic
+start msedge --new-window http://127.0.0.1:8000/ship
+py manage.py runserver
+
+PAUSE
